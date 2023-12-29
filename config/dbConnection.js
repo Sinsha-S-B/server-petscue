5edbd6fa22b167e7245cf4255f536a0d17e9990f
Sinsha S B <sinshasb@gmail.com>
2023-12-29 22:24:00 +0530

    hosting db change

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,9 +1,29 @@
+// import mongoose from 'mongoose';
+// import dotenv from 'dotenv';
+
+// dotenv.config();
+
+// const MONGOOSE = "mongodb://127.0.0.1:27017/Petscue";
+
+//  const connectDB = async () => {
+//     try {
+//         await mongoose.connect(MONGOOSE, {
+//             useNewUrlParser: true,
+//             useUnifiedTopology: true
+//         });
+//         console.log('Database Connected');
+//     } catch (err) {
+//         console.error('Error connecting to database:', err);
+//     }
+// };
+// export default connectDB
+
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-const MONGOOSE = "mongodb://127.0.0.1:27017/Petscue";
+const MONGOOSE = process.env.MONGOOSE;
 
  const connectDB = async () => {
     try {
@@ -16,4 +36,4 @@ const MONGOOSE = "mongodb://127.0.0.1:27017/Petscue";
         console.error('Error connecting to database:', err);
     }
 };
-export default connectDB
+export default connectDB
